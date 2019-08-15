13092e0a1fcf8cde16e8f49d4f26664d00a1b224
johnmcraig <jmcraig32884@gmail.com>
2019-08-15 13:50:21 -0400

    Refaactored Details to new service and added DI

--- TrelloClone/Controllers/CardController.cs
+++ TrelloClone/Controllers/CardController.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TrelloClone.Services;
 using TrelloClone.ViewModel;
 
 namespace TrelloClone.Controllers
 {
     public class CardController : Controller
     {
+        private readonly CardService _cardService;
+
+        public CardController(CardService cardService)
+        {
+            _cardService = cardService;
+        }
+        
         [HttpGet]
         public IActionResult Details(int id)
         {
-            var viewModel = new CardDetails 
-            {
-                Id = id,
-                Contents = "Put the washing out",
-                Notes = "But not if it's raining"
-            };
+            var viewModel = _cardService.GetDetails(id);
 
             return View(viewModel);
         }

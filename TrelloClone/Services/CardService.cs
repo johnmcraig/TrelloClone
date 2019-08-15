c88ccf1f4663fcf6dd755be50eee292f1df9dd46
johnmcraig <jmcraig32884@gmail.com>
2019-08-15 13:53:40 -0400

    Imported DbContext class for DI

--- TrelloClone/Services/CardService.cs
+++ TrelloClone/Services/CardService.cs
@@ -1,7 +1,30 @@
+using System.Linq;
+using TrelloClone.Data;
+using TrelloClone.ViewModel;
+
 namespace TrelloClone.Services
 {
     public class CardService
     {
+        private readonly TrelloCloneDbContext _dbContext;
+
+        public CardService(TrelloCloneDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public CardDetails GetDetails(int id)
+        {
+            var card = _dbContext.Cards.SingleOrDefault(x => x.Id == id);
+            if (card == null) return new CardDetails();
+            
+            return new CardDetails
+            {
+                Id = card.Id,
+                Contents = card.Contents,
+                Notes = card.Notes
+            };
+        }
         
     }
 }

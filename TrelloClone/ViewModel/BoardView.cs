99ec824ecc94ff8cb6e2c04978d1cbe217b76579
johnmcraig <jmcraig32884@gmail.com>
2019-08-15 13:54:17 -0400

    Added Id property to Card then made Lists readonly

--- TrelloClone/ViewModel/BoardView.cs
+++ TrelloClone/ViewModel/BoardView.cs
@@ -8,16 +8,17 @@ namespace TrelloClone.ViewModel
     public class BoardView
     {
         public int Id { get; set; }
-        public List<Column> Columns = new List<Column>();
+        public readonly List<Column> Columns = new List<Column>();
 
         public class Column
         {
             public string Title { get; set; }
-            public List<Card> Cards = new List<Card>();
+            public readonly List<Card> Cards = new List<Card>();
         }
 
         public class Card
         {
+            public int Id { get; set; }
             public string Content { get; set; }
         }
     }

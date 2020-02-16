18cefda842af3deb2fb67fbc2a7e4a90bc43854b
johnmcraig <jmcraig32884@gmail.com>
2020-02-16 18:02:40 -0500

    Removed default comments from scaffold

--- TrelloClone/Startup.cs
+++ TrelloClone/Startup.cs
@@ -17,8 +17,6 @@ namespace TrelloClone
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<BoardService>();
@@ -38,7 +36,6 @@ namespace TrelloClone
                 options.UseSqlServer(connection));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())

46edf9307b3e14b3effe5797cc6abd6579eb848a
johnmcraig <jmcraig32884@gmail.com>
2020-02-18 09:43:11 -0500

    Added required validation to model class, removed from view model

--- TrelloClone/Models/Card.cs
+++ TrelloClone/Models/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace TrelloClone.Models
     public class Card
     {
         public int Id { get; set; }
+        [Required]
         public string Contents { get; set; }
         public string Notes { get; set; }
         public int ColumnId { get; set; }
--- TrelloClone/ViewModel/CardDetails.cs
+++ TrelloClone/ViewModel/CardDetails.cs
@@ -9,7 +9,6 @@ namespace TrelloClone.ViewModel
     public class CardDetails
     {
         public int Id { get; set; }
-        [Required]
         public string Contents { get; set; }
         public string Notes { get; set; }
     }

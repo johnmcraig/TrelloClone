481711fa673a0cf1bd1c0d95e7336d2ea4f24627
johnmcraig <jmcraig32884@gmail.com>
2020-02-18 17:08:36 -0500

    Added column property and SelectListItem to render columns

--- TrelloClone/ViewModel/CardDetails.cs
+++ TrelloClone/ViewModel/CardDetails.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TrelloClone.ViewModel
 {
@@ -11,5 +12,8 @@ namespace TrelloClone.ViewModel
         public int Id { get; set; }
         public string Contents { get; set; }
         public string Notes { get; set; }
+
+        public int Column { get; set; }
+        public List<SelectListItem> Columns { get; set; } = new List<SelectListItem>();
     }
 }

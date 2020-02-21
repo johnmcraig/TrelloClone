295bbb96dc22b6cd9040af8c9a0212ada521fca3
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 20:01:03 -0500

    Implemented Register view model with properties

---
+++ TrelloClone/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrelloClone.ViewModel
+{
+    public class RegisterViewModel
+    {  
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmed password must be the same")]
+        public string ConfirmPassword { get; set; }
+    }
+}

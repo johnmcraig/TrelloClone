bd85d468743b1100f7bb85da184bbc64c1bfe14b
johnmcraig <jmcraig32884@gmail.com>
2020-02-20 20:02:02 -0500

    Created User controller with register, login, and sign out methods

---
+++ TrelloClone/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrelloClone.Infrastructure;
+using TrelloClone.ViewModel;
+
+namespace TrelloClone.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userMAnager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserController(UserManager<AppUser> userMAnager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userMAnager = userMAnager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser 
+                { 
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var result = await _userMAnager
+                    .CreateAsync(user, model.Password);
+
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+
+            return View(model);
+        } 
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Email,
+                    model.Password,
+                    model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}

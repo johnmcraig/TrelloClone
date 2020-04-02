778a5de58a3c971d83f5439e3c7efecfae9c345c
johnmcraig <jmcraig32884@gmail.com>
2020-04-01 22:17:15 -0400

    Created column service class to create and update columns

---
+++ src/TrelloClone/Services/ColumnService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrelloClone.Data;
+
+namespace TrelloClone.Services
+{
+    public class ColumnService
+    {
+        private readonly TrelloCloneDbContext _dbContext;
+
+        public ColumnService(TrelloCloneDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}

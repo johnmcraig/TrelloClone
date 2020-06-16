edc3aad576b4f359f5b927e64fb0a01b31c13066
johnmcraig <jmcraig32884@gmail.com>
2020-06-15 22:29:38 -0400

    Snall convention name change

--- src/Application/Interfaces/IBoardService.cs
+++ src/Application/Interfaces/IBoardService.cs
@@ -3,6 +3,6 @@ namespace Application.Interfaces
     public interface IBoardService : IGenericService
     {
         Task<Board[]> ListAllBoardsAsync();
-        Task<Board> GetBoard(int id);
+        Task<Board> GetBoardAsync(int id);
     }
 }

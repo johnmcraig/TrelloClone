037725847a7657a47384a349c1db9b1df04eee52
johnmcraig <jmcraig32884@gmail.com>
2020-06-15 22:01:12 -0400

    Added Application library to house services implementations for clean architecture

---
+++ src/Application/ColumnService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Persistence;
+using Domain.Entities;
+
+namespace Application
+{
+    public class ColumnService
+    {
+        private readonly TrelloCloneDbContext _dbContext;
+
+        public ColumnService(TrelloCloneDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Column viewModel)
+        {
+        }
+
+        public void Update(Column column)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}

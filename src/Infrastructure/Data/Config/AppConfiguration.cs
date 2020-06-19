344b8935c0e9b3780b2269a23c5e3c7a4508bfb2
johnmcraig <jmcraig32884@gmail.com>
2020-06-19 00:04:12 -0400

    Started foundation of a congif class to create migrations

---
+++ src/Infrastructure/Data/Config/AppConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Infrastructure.Data.Config
+{
+    public class AppConfiguration : IEntityTypeConfiguration<Board>
+    {
+        public void Configure(EntityTypeBuilder<Board> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

a53c5f24dc595a8dca23a5aa5695f45623d65d3f
johnmcraig <jmcraig32884@gmail.com>
2020-07-11 19:23:04 -0400

    Created additional configuration for using SQLite and connecting to a DB

--- src/TrelloClone/Data/TrelloCloneDbContext.cs
+++ src/TrelloClone/Data/TrelloCloneDbContext.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Linq;
+using System.Reflection;
 using TrelloClone.Infrastructure;
 using TrelloClone.Models;
 
@@ -9,13 +13,54 @@ namespace TrelloClone.Data
 {
     public class TrelloCloneDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
     {
-      public TrelloCloneDbContext(DbContextOptions<TrelloCloneDbContext> options)
+      private readonly IConfiguration _config;
+      public TrelloCloneDbContext(DbContextOptions<TrelloCloneDbContext> options, IConfiguration config)
             : base(options)
       {
+          _config = config;
+          Database.EnsureCreated();
       }
 
       public DbSet<Board> Boards { get; set; }
       public DbSet<Card> Cards { get; set; }
+
+      protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            ///<summary>
+            /// Converts decimal to double since it is not supported in SqLite 
+            /// </summary>
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in builder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+                    var dateTimeProperties = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(DateTimeOffset));
+
+                    foreach (var property in properties)
+                    {
+                        builder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+
+                    foreach (var property in dateTimeProperties)
+                    {
+                        builder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion(new DateTimeOffsetToBinaryConverter());
+                    }
+                }
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer(_config.GetConnectionString("sqlConString"));
+            // optionsBuilder.UseInMemoryDatabase(databaseName: "StackPosts");
+            optionsBuilder.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+        }
 
     }
 }

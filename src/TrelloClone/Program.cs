4e7bfba82ca146c22176e491b153a6efe62c21d0
johnmcraig <jmcraig32884@gmail.com>
2021-01-02 12:24:57 -0500

    Added identity, user, and AppUser migration and seed after initial database migration during app startup

--- src/TrelloClone/Program.cs
+++ src/TrelloClone/Program.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TrelloClone.Data;
+using TrelloClone.Infrastructure;
 
 namespace TrelloClone
 {
@@ -29,6 +31,10 @@ namespace TrelloClone
                 {
                     var dbContext = services.GetRequiredService<TrelloCloneDbContext>();
                     await dbContext.Database.MigrateAsync();
+
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = services.GetRequiredService<TrelloCloneDbContext>();
+                    await identityContext.Database.MigrateAsync();
                 }
                 catch (Exception ex)
                 {

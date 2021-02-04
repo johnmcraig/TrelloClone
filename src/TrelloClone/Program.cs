775fdbc45f0288d6b9391bb18595e8eea0a1a022
johnmcraig <jmcraig32884@gmail.com>
2021-02-03 22:13:05 -0500

    Cleaned up unused using statements.

--- src/TrelloClone/Program.cs
+++ src/TrelloClone/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;

35c9399886f5b65d71788280aef17842e34fc01a
johnmcraig <jmcraig32884@gmail.com>
2021-02-05 00:25:49 -0500

    Reset to default, clean Program file as previous version services to create and migrate the database was a conflict with the rest of the application as it was asyncronous while concrete classes were not, thus causing methods to call boards before database was created.

--- src/TrelloClone/Program.cs
+++ src/TrelloClone/Program.cs
@@ -1,44 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using TrelloClone.Data;
-using TrelloClone.Infrastructure;
 
 namespace TrelloClone
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
-                try
-                {
-                    var dbContext = services.GetRequiredService<TrelloCloneDbContext>();
-                    await dbContext.Database.MigrateAsync();
-
-                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                    var identityContext = services.GetRequiredService<TrelloCloneDbContext>();
-                    await identityContext.Database.MigrateAsync();
-                }
-                catch (Exception ex)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
-                }
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

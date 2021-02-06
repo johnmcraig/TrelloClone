8db4cdc20874f0cd4cfcf3dcaaadcca53352908a
johnmcraig <jmcraig32884@gmail.com>
2021-02-05 20:23:56 -0500

    Removed pre-configured migration files. Created a extension method to use IHost to migrate the database.

---
+++ src/TrelloClone/Data/MigrationManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrelloClone.Data
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+             using(var scope = host.Services.CreateScope())
+             {
+                 using(var appContext = scope.ServiceProvider.GetRequiredService<TrelloCloneDbContext>())
+                 {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{ex}");
+                        throw;
+                    }
+                 }
+             }
+
+            return host;
+        }
+    }
+}

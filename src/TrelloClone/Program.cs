d9bea787652eac86ed2e212525d403965105bd0c
johnmcraig <jmcraig32884@gmail.com>
2021-02-05 20:26:19 -0500

    Called the MigrateDatabase extension method after the Build method to apply all migrations and create a database if it do not exist.

--- src/TrelloClone/Program.cs
+++ src/TrelloClone/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using TrelloClone.Data;
 
 namespace TrelloClone
 {
@@ -7,7 +8,10 @@ namespace TrelloClone
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

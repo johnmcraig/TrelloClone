d0ce30dbf542c846f04c293336d5d7ad118c989b
johnmcraig <jmcraig32884@gmail.com>
2021-06-22 00:33:12 -0400

    Added attribute annotations and regular expression so that the title property is required and no blanks are made.

--- src/TrelloClone/ViewModels/NewBoard.cs
+++ src/TrelloClone/ViewModels/NewBoard.cs
@@ -1,7 +1,11 @@
-﻿namespace TrelloClone.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrelloClone.ViewModels
 {
     public class NewBoard
     {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
         public string Title { get; set; }
     }
 }

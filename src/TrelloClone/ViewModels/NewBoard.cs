7910e8ce142322e34988da384edfc6fdcaaa94eb
johnmcraig <jmcraig32884@gmail.com>
2021-06-27 15:22:32 -0400

    Added an error message to display that translates the Regex validation.

--- src/TrelloClone/ViewModels/NewBoard.cs
+++ src/TrelloClone/ViewModels/NewBoard.cs
@@ -4,7 +4,7 @@ namespace TrelloClone.ViewModels
 {
     public class NewBoard
     {
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage ="Please use letters only without special characters and capitalize the first letter.")]
         [Required]
         public string Title { get; set; }
     }
